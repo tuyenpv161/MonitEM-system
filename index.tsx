e1079ad6ed4459fdd8cf23db513d188f4cdee8a9
Tuyen Pham <tuyenpv16@gmail.com>
2026-01-08 15:26:23 +0700

    feat: Convert app to standalone HTML
    Removes React DOM and converts the application to a single, self-contained HTML file for simplified deployment. Includes meta description and Tailwind CSS animations.

--- index.tsx
+++ index.tsx
@@ -1,15 +1,2 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// This file is intentionally left blank as the application has been converted 
+// to a standalone HTML file in index.html for deployment purposes.
